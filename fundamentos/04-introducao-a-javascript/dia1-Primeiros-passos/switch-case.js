ba6b70529c7219f6d086ba5143050a2678e0a728
Caio <caiopadilha@id.uff.br>
2021-11-06 19:04:57 -0300

    exercícios switch case

---
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/switch-case.js
@@ -0,0 +1,15 @@
+let nota = "Aprovada"
+
+switch (nota){
+    case "Aprovada":
+        console.log("Aprovada");
+        break
+    case "Lista":
+        console.log("lista");
+        break
+    case "Reprovada":
+        console.log("Reprovada");
+        break  
+    default:
+    console.log("Valor não identificado")
+}        
