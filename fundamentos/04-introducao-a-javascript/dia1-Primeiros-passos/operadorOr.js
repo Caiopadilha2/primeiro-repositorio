ab77f5d3cd409717a4331c977956c7e6be80d221
Caio <caiopadilha@id.uff.br>
2021-11-06 16:24:01 -0300

    exercícios operadorOr

---
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/operadorOr.js
@@ -0,0 +1,9 @@
+let weekDay = "quarta-feira"
+
+if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira"){
+message = "Oba, mais um dia de aprendizado na Trybe >:D"
+}
+else if (weekDay === "sábado" || weekDay === "domingo"){
+message = "FINALMENTE, descanso merecido UwU"    
+}
+console.log(message)
