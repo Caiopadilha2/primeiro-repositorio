dd96b791afb501ddb7e70772fa249b94dc87ddd6
Caio <caiopadilha@id.uff.br>
2021-11-07 14:55:07 -0300

    Adicionei enunciado exercício 9

--- fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/exercicio9.js
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/exercicio9.js
@@ -1,4 +1,5 @@
-
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if .
 
 const a = 3;
 const b = 5;
