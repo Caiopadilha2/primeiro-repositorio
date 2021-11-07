e0c5207e8ff51d97cdecf43511008d3c062f6d8c
Caio <caiopadilha@id.uff.br>
2021-11-07 11:24:29 -0300

    Exercício 6

---
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/exercicio6.js
@@ -0,0 +1,19 @@
+const peça;
+
+switch (peça.toLowerCase()) {
+    case "rei":
+        console.log("O rei pode mover-se em todas as direções");
+        break
+    case "rainha":
+        console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
+        break
+    case "bispo":
+        console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.")
+        break
+    case "cavalo":
+        console.log("O movimento do cavalo é em forma de “L”")
+        break;
+    default:
+    console.log("Não é uma peça de xadrez")
+    break;
+}
