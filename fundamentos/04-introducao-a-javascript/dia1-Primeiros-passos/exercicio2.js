09dc19dba272cf41f8d35ac1a6ac0e93277ebca9
Caio <caiopadilha@id.uff.br>
2021-11-07 10:56:14 -0300

    exercícios 3, 4 e 5

---
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/exercicio2.js
@@ -0,0 +1,11 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+const a = 5;
+const b = 10;
+
+if (a > b) {
+console.log("A maior que B")
+}
+else {
+console.log("B maior que A")
+}
---
+++ fundamentos/04-introducao-a-javascript/dia1-Primeiros-passos/exercicio3.js
@@ -0,0 +1,15 @@
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const a = 5;
+const b = 10;
+const c = 15;
+
+if ((a > b) && (a > c)){
+    console.log("A é o maior")
+}
+else if ((b > a) && (b > c)){
+    console.log("B é o maior")
+}
+else {
+    console.log("C é o maior")
+}
