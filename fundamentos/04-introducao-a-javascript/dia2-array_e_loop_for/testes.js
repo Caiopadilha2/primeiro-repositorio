7891da1a93b99dd9451a8b80a5d3fc00f1fe9f70
Caio <caiopadilha@id.uff.br>
2021-11-08 14:46:21 -0300

    Testes

---
+++ fundamentos/04-introducao-a-javascript/dia2-array_e_loop_for/testes.js
@@ -0,0 +1,6 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+tasksList.push("Estudar")
+console.log (tasksList)
+console.log(tasksList.length)
+tasksList.unshift("Acordar")
+console.log(tasksList)
