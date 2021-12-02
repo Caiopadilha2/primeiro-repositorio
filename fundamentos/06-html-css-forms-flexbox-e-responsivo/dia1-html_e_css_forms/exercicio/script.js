6174b23a5cbb137a91e41f5b8a7e2d8b185e28ad
Caio <caiopadilha@id.uff.br>
2021-12-02 11:59:36 -0300

    Validando nome

--- fundamentos/06-html-css-forms-flexbox-e-responsivo/dia1-html_e_css_forms/exercicio/script.js
+++ fundamentos/06-html-css-forms-flexbox-e-responsivo/dia1-html_e_css_forms/exercicio/script.js
@@ -1,12 +1,34 @@
-
 function createStateOptions() {
-    let states = document.getElementById('state');
-    let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
-  
-    for (let index = 0; index < stateOptions.length; index += 1) {
-      let option = document.createElement('option');
-      option.innerText = stateOptions[index];
-      option.value = stateOptions[index];
-      states.appendChild(option);
-    }
+  let states = document.getElementById('state');
+  let stateOptions = ['AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    let option = document.createElement('option');
+    option.innerText = stateOptions[index];
+    option.value = stateOptions[index];
+    states.appendChild(option);
   }
+}
+
+function validacaoDados(){
+  let name = document.querySelector('[name=name]');
+  if(name.value.length > 40 || name.value.length === 0){
+    alert('Nome inválido');
+  }
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  validacaoDados();
+  
+  
+}
+
+window.onload = function () {
+  createStateOptions();
+
+  let submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', handleSubmit);
+
+
+}
