1c5cdc3ea5bb3c095bd4169eef36a18502753b34
Caio <caiopadilha@id.uff.br>
2022-02-01 15:41:42 -0300

    Pokedex nao finalizada

---
+++ 02-front_end/bloco-10-introducao-a-react/my-pokedex/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
+
+function App() {
+  return (
+   
+  );
+}
+
+export default App;
